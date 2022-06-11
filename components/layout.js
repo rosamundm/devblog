b3cb5bf992ad5730e9668326058c5b7db33aa490
Rosamund Mather <rsmnd.mthr@gmail.com>
2022-06-11 23:21:20 +0200

    Set up syntax highlighting incl. sexy Laserwave theme

--- components/layout.js
+++ components/layout.js
@@ -1,8 +1,15 @@
-import Alert from '../components/alert'
-import Footer from '../components/footer'
-import Meta from '../components/meta'
+import Prism from "Prismjs";
+import { useEffect } from "react";
+import Alert from "../components/alert";
+import Footer from "../components/footer";
+import Meta from "../components/meta";
 
 export default function Layout({ preview, children }) {
+
+  useEffect(() => {
+    Prism.highlightAll();
+  }, []);
+  
   return (
     <>
       <Meta />
