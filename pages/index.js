42ed16a835e76bca7c94e24886f9717a13624932
Rosamund Mather <rsmnd.mthr@gmail.com>
2022-06-12 19:30:39 +0200

    Change intro title

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Index({ allPosts }) {
     <>
       <Layout>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>Rosamund Mather</title>
         </Head>
         <Container>
           <Intro />
