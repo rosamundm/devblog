7b65dfed0df721fdb465af2839b46a063fa28eea
Rosamund Mather <rsmnd.mthr@gmail.com>
2022-06-12 17:42:31 +0200

    Add pages to production build

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -11,6 +11,7 @@ import Head from 'next/head'
 import { CMS_NAME } from '../../lib/constants'
 import markdownToHtml from '../../lib/markdownToHtml'
 
+
 export default function Post({ post, morePosts, preview }) {
   const router = useRouter()
   if (!router.isFallback && !post?.slug) {
@@ -29,11 +30,9 @@ export default function Post({ post, morePosts, preview }) {
                 <title>
                   {post.title} | Next.js Blog Example with {CMS_NAME}
                 </title>
-                <meta property="og:image" content={post.ogImage.url} />
               </Head>
               <PostHeader
                 title={post.title}
-                coverImage={post.coverImage}
                 date={post.date}
               />
               <PostBody content={post.content} />
