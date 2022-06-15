51df44cc7a3f1377de245df1b71157a5a06d3aa7
Rosamund Mather <rsmnd.mthr@gmail.com>
2022-06-15 21:53:05 +0200

    Add fontawesome icons and emoji favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import "../styles/index.css"
 import "../styles/prism-laserwave.css";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+
+config.autoAddCss = false;
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
