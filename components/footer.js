27db489e61b08b4a020a0c40e2eef136b0c74a43
Rosamund Mather <rsmnd.mthr@gmail.com>
2022-06-15 19:44:06 +0200

    Strip down footer, header, intro

--- components/footer.js
+++ components/footer.js
@@ -9,25 +9,22 @@ export default function Footer() {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <div className="text-2xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            <Link href={`/about/`}>about</Link> {''}
-            {/* <Link href={`/reading/`}>reading</Link> {''} */}
-            {/* <Link href={`/resources/`}>resources</Link> {''} */}
-            {/* <Link href={`/contact/`}>contact</Link> */}
-            <Link href={`/impressum/`}>impressum</Link>
+            <a
+              href="/impressum/">
+              © Rosamund Mather {(new Date().getFullYear())} {''}
+            </a>
           </div>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <a
-              href="https://help-ua-volunteers.com/"
-              className="mx-3 text-center bg-black hover:bg-white hover:text-black border no-underline border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              🇺🇦 Help Ukrainian volunteers!
-            </a>
             <div
               className="mx-3 text-sm"
             >
-              © Rosamund Mather {(new Date().getFullYear())} {''}
+              <a
+                href="https://help-ua-volunteers.com/"
+                className="mx-3 text-center bg-black hover:bg-white hover:text-black border no-underline border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              >
+                🇺🇦 Help Ukrainian volunteers!
+              </a>
             </div>
-            
           </div>
         </div>
       </Container>
--- components/intro.js
+++ components/intro.js
@@ -3,11 +3,13 @@ import Link from "next/link";
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-mono tracking-tighter leading-tight md:pr-8 drop-shadow-lg">
-        rosamund.dev
+      <h1 className="text-6xl md:text-8xl font-mono tracking-tighter leading-tight md:pr-8 drop-shadow-lg no-underline">
+          rosamund.dev
       </h1>
-      <h4 className="text-right md:text-left text-lg mt-5 md:pl-8">
-       
+      <h4 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
+        <Link href={"/about/"}>
+          about⟶
+        </Link>
       </h4>
     </section>
   )
