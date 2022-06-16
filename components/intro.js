c9b6d5e25e9774fdfea6b328d465056efd1e5fbd
Rosamund Mather <rsmnd.mthr@gmail.com>
2022-06-16 22:19:53 +0200

    Change intrp

--- components/intro.js
+++ components/intro.js
@@ -8,9 +8,16 @@ export default function Intro() {
       </h1>
       <h4 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
         <Link href={"/about/"}>
-          about⟶
+          who? why? ⟶
         </Link>
       </h4>
+      {/*
+      <h4 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
+        <Link href={"/reading-2022/"}>
+          reading⟶
+        </Link>
+      </h4>
+      */}
     </section>
   )
 }
