54852ded7b502e9069e5f91c21be0aa6af122f4c
Rosamund Mather <rsmnd.mthr@gmail.com>
2022-06-16 21:40:02 +0200

    Add reading 2022 and 2021

---
+++ pages/reading-2022.js
@@ -0,0 +1,118 @@
+import Head from 'next/head'
+import Container from '../components/container';
+import Header from '../components/header';
+import Layout from '../components/layout';
+
+export default function Reading2022() {
+  return (
+    <>
+    <Layout>
+      <Container>
+      <Header />
+      <article className="mb-32">
+          <Head>
+              <title>
+                Reading 2022
+              </title>
+          </Head>
+
+          <div className="max-w-2xl mx-auto">
+
+              <h2 className="text-3xl">
+                What I'm reading in 2022
+              </h2>
+                  <br></br>
+
+              <ol>
+                <li data-block-key="fp1iu">
+                  <b>Devon Price</b> - <i>Laziness Does Not Exist</i> (5/5)</li>
+                <li data-block-key="bmhgb">
+                  <b>Andreas Malm</b> - <i>How to Blow Up a Pipeline</i> (3/5)
+                </li>
+                <li data-block-key="aclnu">
+                  <b>Erika Fatland (tr. Kari Dickson)</b> - <a href="https://www.goodreads.com/review/show/3091449226"><i>Sovietistan: Travels in Turkmenistan, Kazakhstan, Tajikistan, Kyrgyzstan, and Uzbekistan</i></a> (2/5)
+                </li>
+                <li data-block-key="bdi4t">
+                  <b>Pik-Shuen Fung</b> - <i>Ghost Forest</i> (3/5)
+                </li>
+                <li data-block-key="4gr0f">
+                  <b>Yevgenia Belorusets (tr. Claudia Dathe)</b> - <i>Glückliche Fälle</i> (3/5)
+                </li>
+                <li data-block-key="ev9op">
+                  <b>Mina Seçkin</b> - <i>The Four Humors</i> (4/5)
+                </li>
+                <li data-block-key="btk2p">
+                  <b>Samra Habib</b> - <i>We Have Always Been Here: A Queer Muslim Memoir</i> (2/5)
+                </li>
+                <li data-block-key="vavi">
+                  <b>Albert Costa (tr. John W. Schwieter)</b> - <a href="https://www.goodreads.com/review/show/3356079879"><i>The Bilingual Brain: And What It Tells Us About the Science of Language</i></a> (2/5)
+                </li>
+                <li data-block-key="450kj">
+                  <b>Jane Sanderson</b> - <i>Mix Tape</i> (2/5)
+                </li>
+                <li data-block-key="9qu5m">
+                  <b>Haruki Murakami (tr. Philip Gabriel)</b> - <i>South of the Border, West of the Sun</i> (3/5)
+                </li>
+                <li data-block-key="f30qv">
+                  <b>Kate Briggs</b> - <a href="https://www.goodreads.com/review/show/2365532018"><i>This Little Art</i></a> (4/5)
+                </li>
+                <li data-block-key="dsjp4">
+                  <b>Amy Liptrot</b> - <i>The Instant</i> (4/5)
+                </li>
+                <li data-block-key="alol9">
+                  <b>Ellen Ullman</b> - <i>Close to the Machine: Technophilia and Its Discontents</i> (2/5)
+                </li>
+                <li data-block-key="2l6kn">
+                  <b>Leonard Cohen</b> - <i>The Flame: Poems and Selections from Notebooks</i> (3/5)
+                </li>
+                <li data-block-key="b90he">
+                  <b>Devon Price</b> - <i>Unmasking Autism: Discovering the New Faces of Neurodiversity</i> (4/5)
+                </li>
+                <li data-block-key="4d7ct">
+                  <b>Ingrid Persaud</b> - <i>Love After Love</i> (3/5)
+                </li>
+                <li data-block-key="9uahg">
+                  <b>Esther Farmer, Rosalind Petchesky (eds.)</b> - <a href="https://www.goodreads.com/review/show/4605822941"><i>A Land with a People: Palestinians and Jews Confront Zionism</i></a> (5/5)
+                </li>
+                <li data-block-key="77j0h">
+                  <b>Kirsty Bell</b> - <a href="https://www.goodreads.com/review/show/4620943940"><i>The Undercurrents</i></a> (3/5)
+                </li>
+                <li data-block-key="fl5r0">
+                  <b>Rachel Bloom</b> - <i>I Want to Be Where the Normal People Are</i> (3/5)
+                </li>
+                <li data-block-key="454ai">
+                  <b>Caitlin Barasch</b> - <a href="https://www.goodreads.com/review/show/4461697072"><i>A Novel Obsession</i></a> (4/5)
+                </li>
+                <li data-block-key="ar5q0">
+                  <b>Aisha Sabatini Sloan</b> - <i>Borealis</i> (3/5)
+                </li>
+                <li data-block-key="135u9">
+                  <b>Mary Oliver</b> - <i>A Poetry Handbook</i> (4/5)
+                </li>
+                <li data-block-key="9d14m">
+                  <b>Gargi Bhattacharyya, Adam Elliott-Cooper et al</b> - <i>Empire&#x27;s Endgame: Racism and the British State</i> (5/5)
+                </li>
+              </ol>
+
+              <br></br>
+
+              <h2 className="text-3xl">
+                <a href="/reading-2021">2021</a> | 
+                2020 |
+                2019 |
+                2018 |
+                2017 |
+                2016 |
+                2015 |
+                2014 |
+                2013 |
+                2012 |
+              </h2>
+          </div>
+      </article>
+        
+      </Container>
+    </Layout>
+    </>
+  )
+}
