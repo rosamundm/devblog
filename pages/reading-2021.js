5f7ae9955c28fa3205d5111135d08e7858c0bb7d
Rosamund Mather <rsmnd.mthr@gmail.com>
2022-07-04 22:19:25 +0200

    Add line spacing to reading 2021/2022

--- pages/reading-2021.js
+++ pages/reading-2021.js
@@ -27,138 +27,191 @@ export default function Reading2021() {
     <li data-block-key="5nmmp">
         <b>Mieko Kawakami (tr. Sam Bett, David Boyd)</b> - <a href="https://www.goodreads.com/review/show/3207549983"><i>Breasts and Eggs</i></a> (2/5)
     </li>
+    <br></br>
     <li data-block-key="3qnsl">
         <b>Gérald Leblanc</b> - <i>Moncton Mantra</i> (5/5) ⭐️
     </li>
+    <br></br>
     <li data-block-key="35snf">
         <b>Amber Scorah</b> - <i>Leaving the Witness: Exiting a Religion and Finding a Life</i> (4/5)
     </li>
+    <br></br>
     <li data-block-key="9n6f3">
         <b>Yaa Gyasi</b> - <a href="https://www.goodreads.com/review/show/3805847747"><i>Transcendent Kingdom</i></a> (3/5)
     </li>
+    <br></br>
     <li data-block-key="1qpoj">
         <b>Tatiana Ryckman</b> - <i>I Don&#x27;t Think of You (Until I Do)</i> (3/5)
     </li>
+    <br></br>
     <li data-block-key="9mrma">
         <b>Domenica Degrandis</b> - <i>Making Work Visible: Exposing Time Theft to Optimize Work &amp; Flow</i> (3/5)
     </li>
+    <br></br>
     <li data-block-key="45ld">
         <b>Melissa Broder</b> - <a href="https://www.goodreads.com/review/show/3420181975"><i>Milk Fed</i></a> (5/5) ⭐️
     </li>
+    <br></br>
     <li data-block-key="45gij">
         <b>Carolyn Marie Souaid</b> - <i>Yasmeen Haddad Loves Joanasi Maqaittik</i> (3/5)
     </li>
+    <br></br>
     <li data-block-key="mcba">
         <b>Joshua Whitehead</b> - <a href="https://www.goodreads.com/review/show/3624070857"><i>Jonny Appleseed</i></a> (4/5)
     </li>
+    <br></br>
     <li data-block-key="4f1ma">
         <b>Lauren Oyler</b> - <a href="https://www.goodreads.com/review/show/3605982078"><i>Fake Accounts</i></a> (3/5)
     </li>
+    <br></br>
     <li data-block-key="6e7mc">
         <b>Judith Schalansky</b> - <a href="https://www.goodreads.com/review/show/3527915912"><i>Atlas der abgelegenen Inseln: 50 Inseln, auf denen ich nie war und niemals sein werde</i></a> (2/5)
     </li>
+    <br></br>
     <li data-block-key="8ph0l">
         <b>J. Ryan Stradal</b> - <a href="https://www.goodreads.com/review/show/3897810301"><i>The Lager Queen of Minnesota</i></a> (4/5)
     </li>
+    <br></br>
     <li data-block-key="epsn">
         <b>Olga Grjasnowa</b> - <i>Der Russe ist einer, der Birken liebt</i> (3/5)
     </li>
+    <br></br>
     <li data-block-key="4tkmp">
         <b>Harriet Tyce</b> - <i>Blood Orange</i> (4/5)
     </li>
+    <br></br>
     <li data-block-key="2t5ri">
         <b>Torrey Peters</b> - <i>Detransition, Baby</i> (3/5)
     </li>
+    <br></br>
     <li data-block-key="8qf8">
         <b>Richard Wright</b> - <a href="https://www.goodreads.com/review/show/957375890"><i>Native Son</i></a> (4/5)
     </li>
+    <br></br>
     <li data-block-key="eln07">
         <b>Roberto Bolaño (tr. Natasha Wimmer)</b> - <a href="https://www.goodreads.com/review/show/1063421001"><i>The Savage Detectives</i></a> (3/5)
     </li>
+    <br></br>
     <li data-block-key="215t9">
         <b>Chelsey Johnson</b> - <a href="https://www.goodreads.com/review/show/2311794924"><i>Stray City</i></a> (5/5) ⭐️
     </li>
+    <br></br>
     <li data-block-key="5an2b">
         <b>Noam Chomsky &amp; Ilan Pappé</b> - <i>On Palestine</i> (4/5)
     </li>
+    <br></br>
     <li data-block-key="4gptu">
         <b>Semezdin Mehmedinović (tr. Celia Hawkesworth)</b> - <a href="https://www.goodreads.com/review/show/4030167706"><i>My Heart</i></a> (4/5)
     </li>
+    <br></br>
     <li data-block-key="dc4oe">
         <b>Laura McPhee-Browne</b> - <i>Cherry Beach</i> (4/5)
     </li>
+    <br></br>
     <li data-block-key="66bam">
       <b>Alexandria Bellefleur</b> - <a href="https://www.goodreads.com/review/show/4058161095"><i>Written in the Stars</i></a>(3/5)
     </li>
+    <br></br>
     <li data-block-key="9kkqf">
         <b>Tasha Spillett &amp; Natasha Donovan</b> - <a href="https://www.goodreads.com/review/show/3605828575"><i>From the Roots Up</i></a> (4/5)
     </li>
-    <li data-block-key="5tt9d"><b>Joya Goffney</b> - <i>Excuse Me While I Ugly Cry</i> (4/5)</li>
-    <li data-block-key="5f6m1"><b>Danielle Evans </b>- <i>The Office of Historical Corrections</i> (3/5)</li>
-    <li data-block-key="850ab"><b>Marcy Dermansky</b> - <i>Very Nice</i> (2/5)</li>
+    <br></br>
+    <li data-block-key="5tt9d">
+        <b>Joya Goffney</b> - <i>Excuse Me While I Ugly Cry</i> (4/5)
+    </li>
+    <br></br>
+    <li data-block-key="5f6m1">
+        <b>Danielle Evans </b>- <i>The Office of Historical Corrections</i> (3/5)
+    </li>
+    <br></br>
+    <li data-block-key="850ab">
+        <b>Marcy Dermansky</b> - <i>Very Nice</i> (2/5)
+    </li>
+    <br></br>
     <li data-block-key="d1vqc">
         <b>Susan Abulhawa</b> - <a href="https://www.goodreads.com/review/show/4121575190"><i>Against the Loveless World</i></a> (4/5)
     </li>
+    <br></br>
     <li data-block-key="dm2kd">
         <b>Kikuko Tsumura (tr. Polly Barton) </b>- <a href="https://www.goodreads.com/review/show/3624077306"><i>There&#x27;s No Such Thing As An Easy Job</i></a> (2/5)
     </li>
+    <br></br>
     <li data-block-key="didff">
         <b>Amy Feltman</b> - <a href="https://www.goodreads.com/review/show/2787448698"><i>Willa &amp; Hesper</i></a> (4/5)
     </li>
+    <br></br>
     <li data-block-key="ducth">
         <b>Dahlia Adler</b> - <a href="https://www.goodreads.com/review/show/4145530462"><i>Cool for the Summer</i></a>(3/5)
     </li>
+    <br></br>
     <li data-block-key="4qr8o">
         <b>Curtis Sittenfeld</b> - <a href="https://www.goodreads.com/review/show/3243340465"><i>Rodham</i></a> (3/5)
     </li>
+    <br></br>
     <li data-block-key="c6n8g">
         <b>Leanne Betasamosake Simpson</b> - <a href="https://www.goodreads.com/review/show/1747100643"><i>Islands of Decolonial Love</i></a> (3/5 )
     </li>
+    <br></br>
     <li data-block-key="j4sv">
         <b>Helen Corner-Bryant &amp; Kathryn Price</b> - <i>On Editing: How to Edit Your Novel the Professional Way</i> (4/5)
     </li>
+    <br></br>
     <li data-block-key="9q6b">
         <b>Olga Grjasnowa</b> - <a href="https://www.goodreads.com/review/show/4063097228"><i>Die Macht der Mehrsprachigkeit: Über Herkunft und Vielfalt</i></a> (4/5)
     </li>
+    <br></br>
     <li data-block-key="f7lnh">
         <b>Olivia Sudjic</b> - <i> Exposure</i> (5/5) ⭐️
     </li>
+    <br></br>
     <li data-block-key="15bnd">
         <b>Eva Hoffman</b> - <i>Lost in Translation: A Life in a New Language</i> (3/5)
     </li>
+    <br></br>
     <li data-block-key="4nlct">
         <b>Dana Czapnik</b> - <i>The Falconer</i> (3/5)
     </li>
+    <br></br>
     <li data-block-key="5ja9u">
         <b>Matthias Roberts</b> - <a href="https://www.goodreads.com/user/show/15102850-rosamund"><i>Beyond Shame: Creating a Healthy Sex Life on Your Own Terms</i></a> (3/5)
     </li>
+    <br></br>
     <li data-block-key="dsi9">
-        <b>Krys Malcolm Belc</b> - <i>The Natural Mother of the Child: A Memoir of Non-Binary Pa renthood</i> (3/5)
+        <b>Krys Malcolm Belc</b> - <i>The Natural Mother of the Child: A Memoir of Non-Binary Parenthood</i> (3/5)
     </li>
+    <br></br>
     <li data-block-key="3eba5">
         <b>Sally Rooney</b> - <i>Beautiful World, Where Are You?</i> (3/5)
     </li>
+    <br></br>
     <li data-block-key="cueep">
         <b>Daniel Bowman Jr</b>. - <i>On the Spectrum: Autism, Faith, and the Gifts of Neurodiversity</i> (4/5)
     </li>
+    <br></br>
     <li data-block-key="7pvej">
         <b>Jean Hanff Korelitz</b> - <a href="https://www.goodreads.com/review/show/4126703460"><i>The Plot</i></a> (3/5)
     </li>
+    <br></br>
     <li data-block-key="118ml">
         <b>Anne Applebaum</b> - <i>Red Famine: Stalin&#x27;s War on Ukraine</i> (4/5)
     </li>
+    <br></br>
     <li data-block-key="3lii6">
         <b>Jen Winston</b> - <i>Greedy: Notes from a Bisexual Who Wants Too Much</i> (3/5)
     </li>
+    <br></br>
     <li data-block-key="4jkka">
         <b>Kerri Arsenault</b> - <i>Mill Town: Reckoning With What Remains</i> (4/5)
     </li>
+    <br></br>
     <li data-block-key="c18f3">
         <b>SJ Sindu</b> - <i>Marriage of a Thousand Lies</i> (4/5)
-        </li>
+    </li>
+    <br></br>
     <li data-block-key="5soo1">
         <b>Adrian Duncan</b> - <i>Love Notes from a German Building Site</i> (2/5)
     </li>
+    <br></br>
     <li data-block-key="a4fmo">
         <b>Daphné B. (tr. Alex Manley)</b> - <a href="https://www.goodreads.com/review/show/4106311625"><i>Made-Up: A True Story of Beauty Culture Under Late Capitalism</i></a> (4/5)
     </li>
