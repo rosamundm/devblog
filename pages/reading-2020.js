4fc93063aad0051bbaa1828e2f547408485e8d6a
Rosamund Mather <rsmnd.mthr@gmail.com>
2022-07-05 09:14:10 +0200

    Add reading 2020

---
+++ pages/reading-2020.js
@@ -0,0 +1,294 @@
+import Head from 'next/head'
+import Container from '../components/container';
+import Header from '../components/header';
+import Layout from '../components/layout';
+
+export default function Reading2020() {
+  return (
+    <>
+    <Layout>
+      <Container>
+      <Header />
+      <article className="mb-32">
+
+          <Head>
+              <title>
+              What I read in 2020
+              </title>
+          </Head>
+          <div className="max-w-2xl mx-auto">
+              <h2 className="text-3xl">
+                What I read in 2020
+              </h2>
+              
+              <br></br>
+
+              <ol>
+                <li data-block-key="luguh">
+                    <b>Ottessa Moshfegh</b> - <a href="https://www.goodreads.com/review/show/2398151652"><i>My Year of Rest and Relaxation</i></a> (3/5)
+                </li>
+                <br></br>
+                <li data-block-key="wt61s">
+                    <b>Juno Mac &amp; Molly Smith</b> - <a href="https://www.goodreads.com/review/show/3132365496"><i>Revolting Prostitutes: The Fight for Sex Workers&#x27; Rights</i></a> (5/5)
+                </li>
+                <br></br>
+                <li data-block-key="y8pdq">
+                    <b>Curtis Sittenfeld</b> - <a href="https://www.goodreads.com/review/show/3143627995"><i>Eligible</i></a> (4/5)
+                </li>
+                <br></br>
+                <li data-block-key="9ece9">
+                    <b>Helon Habila</b> - <a href="https://www.goodreads.com/review/show/2872480556"><i>Travellers</i></a> (4/5)
+                </li>
+                <br></br>
+                <li data-block-key="3q2r2">
+                    <b>Ling Ma</b> - <a href="https://www.goodreads.com/review/show/2525292696"><i>Severance</i></a> (4/5)
+                </li>
+                <br></br>
+                <li data-block-key="o99pz">
+                    <b>Jia Tolentino</b> - <a href="https://www.goodreads.com/review/show/3187217065"><i>Trick Mirror: Reflections on Self-Delusion</i></a> (2/5)
+                </li>
+                <br></br>
+                <li data-block-key="quu00">
+                    <b>Paul Scraton</b> - <a href="https://www.goodreads.com/review/show/2525333871"><i>Built on Sand</i></a> (3/5)
+                </li>
+                <br></br>
+                <li data-block-key="ldsas">
+                    <b>Dave Hoover &amp; Adewale Oshineye</b> - <a href="https://www.goodreads.com/review/show/3205062797"><i>Apprenticeship Patterns: Guidance for the Aspiring Software Craftsman</i></a> (4/5)
+                </li>
+                <br></br>
+                <li data-block-key="6vg5a">
+                    <b>Ocean Vuong</b> - <a href="https://www.goodreads.com/review/show/3207463967"><i> On Earth We&#x27;re Briefly Gorgeous</i></a> (2/5)
+                </li>
+                <br></br>
+                <li data-block-key="59xbw">
+                    <b>Bassey Ikpi</b> - <a href="https://www.goodreads.com/review/show/2908850163"><i>I'm Telling the Truth, But I&#x27;m Lying</i></a> (3/5)
+                </li>
+                <br></br>
+                <li data-block-key="clc5x">
+                    <b>Natalia Ginzburg (tr. Frances Frenaye)</b> - <a href="https://www.goodreads.com/review/show/3227949923"><i>The Dry Heart</i></a> (4/5)
+                </li>
+                <br></br>
+                <li data-block-key="u8skd">
+                    <b>Stephen King</b> - <a href="https://www.goodreads.com/review/show/2225137522"><i>On Writing: A Memoir of the Craft</i></a> (4/5)
+                </li>
+                <br></br>
+                <li data-block-key="td2d3">
+                    <b>Anna Reid</b> - <a href="https://www.goodreads.com/review/show/2855267354"><i>Borderland: A Journey Through the History of Ukraine</i></a> (2/5)
+                </li>
+                <br></br>
+                <li data-block-key="3idfp">
+                    <b>Abigail Melton & Lilith Cooper</b> - <a href="https://www.goodreads.com/review/show/3163362136"><i>Gears for Queers</i></a> (5/5)
+                </li>
+                <br></br>
+                <li data-block- key="ehqk2">
+                    <b>Anna Wiener</b> - <a href="https://www.goodreads.com/review/show/3263805256"><i>Uncanny Valley</i></a> (4/5)
+                </li>
+                <br></br>
+                <li data-block-key="1nipx">
+                    <b>Oyinkan Braithwaite</b> - <a href="https://www.goodreads.com/review/show/2598113034"><i>My Sister, the Serial Killer</i></a> (2/5)
+                </li>
+                <br></br>
+                <li data-block-key="8hgo8">
+                    <b>Juliet Blaxland</b> - <a href="https://www.goodreads.com/review/show/3091446505"><i>The Easternmost House</i></a> (2/5)
+                </li>
+                <br></br>
+                <li data-block-key="3bc9u">
+                    <b>Deane Barker</b> - <a href="https://www.goodreads.com/review/show/3285562513"><i>Web Content Management: Systems, Features, and Best Practices</i></a> (4/5)
+                </li>
+                <br></br>
+                <li data-block-key="v2z8d">
+                    <b>Margarete Stokowski</b> - <a href="https://www.goodreads.com/review/show/3295210374"><i>Untenrum frei</i></a> (3/5)
+                </li>
+                <br></br>
+                <li data-block-key="cvuhk">
+                    <b>Beth O'Leary</b> - <a href="https://www.goodreads.com/review/show/3299181378"><i>The Flatshare</i></a> (2/5)
+                </li>
+                <br></br>
+                <li data-block-key="5z5al">
+                    <b>Saeed Jones</b> - <a href="https://www.goodreads.com/review/show/3302115041"><i>How We Fig ht For Our Lives</i></a> (4/5)
+                </li>
+                <br></br>
+                <li data-block-key="b6fm4">
+                    <b>Niviaq Korneliussen (tr. Anna Halager)</b> - <a href="https://www.goodreads.com/review/show/2936840251"><i>Crimson</i></a> (3/5)
+                </li>
+                <br></br>
+                <li data-block-key="m3hwr">
+                    <b>Jill Stark</b> - <a href="https://www.goodreads.com/review/show/2474189897"><i>Happy Never After: Why the Happiness Fairytale is Driving Us Mad (And How I Flipped the Script)</i></a> (3/5)
+                </li>
+                <br></br>
+                <li data-block-key="p5cfg">
+                    <b>Samantha Irby</b> - <a href="https://www.goodreads.com/review/show/3339603986"><i>Wow, No Thank You.: Essays</i></a> (3/5)
+                </li>
+                <br></br>
+                <li data-block-key="s6bn6">
+                    <b>Andrea Bartz</b> - <a href="https://www.goodreads.com/review/show/3132373689"><i>The Herd</i></a> (3/5)
+                </li>
+                <br></br>
+                <li data-block-key="abgh0">
+                    <b>Mary H. K. Choi</b> - <a href="https://www.goodreads.com/review/show/3362835549"><i>Emergency Contact</i></a> (3/5)
+                </li>
+                <br></br>
+                <li data-block-key="7h5jv">
+                    <b>Kiley Reid</b> - <a href="https://www.goodreads.com/review/show/3368353916"><i>Such a Fun Age</i> </a> (5/5)
+                </li>
+                <br></br>
+                <li data-block-key="bpnas">
+                    <b>Kim Ritter &amp; Heinz-Jürgen Voß</b> - <a href="https://www.goodreads.com/review/show/2985142900"><i>Being Bi: Bisexualität zwischen Unsichtbarkeit und Chic</i></a> (3/5)
+                </li>
+                <br></br>
+                <li data-block-key="7fo73">
+                    <b>Bethany Rutter</b> - <a href="https://www.goodreads.com/review/show/3382603467"><i>No Big Deal</i></a> (3/5)
+                </li>
+                <br></br>
+                <li data-block-key="w2foe">
+                    <b>Akwaeke Emezi</b> - <a href="https://www.goodreads.com/review/show/3393521881"><i>Freshwater</i></a> (3/5)
+                </li>
+                <br></br>
+                <li data-block-key="xsucg">
+                    <b>Xiuhtezcatl Martinez</b> - <a href="https://www.goodreads.com/review/show/3398675856"><i>Imaginary Borders</i></a> (3/5)
+                </li>
+                <br></br>
+                <li data-block-key="p46a4">
+                    <b>Mikki Kendall</b> - <a href="https://www.goodreads.com/review/show/3371868436"><i>Hood Feminism: Notes from the Women White Feminists Forgot</i></a> (3/5)
+                </li>
+                <br></br>
+                <li da ta-block-key="fr97u">
+                    <b>Kate Elizabeth Russell</b> - <a href="https://www.goodreads.com/review/show/3430958979"><i>My Dark Vanessa</i></a> (4/5)
+                </li>
+                <br></br>
+                <li data-block-key="9sh2g">
+                    <b>Elizabeth Acevedo</b> - <a href="https://www.goodreads.com/review/show/3443495895"><i>The Poet X</i></a> (4/5)
+                </li>
+                <br></br>
+                <li data-block-key="h3fpe">
+                    <b>Deborah Feldman</b> - <a href="https://www.goodreads.com/review/show/3450703027"><i>Unorthodox: The Scandalous Rejection of My Hasidic Roots</i></a> (3/5)
+                </li>
+                <br></br>
+                <li data-block-key="xi55r">
+                    <b>Naoise Dolan</b> - <a href="https://www.goodreads.com/review/show/3082090218"><i>Exciting Times</i></a> (2/5)
+                </li>
+                <br></br>
+                <li data-block-key="ex8kq">
+                    <b>Emily Henry</b> - <a href="https://www.goodreads.com/review/show/3257823473"><i>Beach Read</i></a> (3/5)
+                </li>
+                <br></br>
+                <li data-block-key="mbn8e">
+                    <b>Ben Fergusson</b> - <a href="https://www.goodreads.com/review/show/3462560697"><i>An Honest Man</i></a> (5/5)
+                </li>
+                <br></br>
+                <li data-block-key="k5ogf">
+                    <b>Carmen Maria Machado</b> - <a href="https://www.goodreads.com/review/show/3485086851"><i>In the Dream House</i></a> (4/5)
+                </li>
+                <br></br>
+                <li data-block-key="l34c6">
+                    <b>Paula Irmschler</b> - <a href="https://www.goodreads.com/review/show/3276131954"><i>Superbus en</i></a> (4/5)
+                </li>
+                <br></br>
+                <li data-block-key="r534y">
+                    <b>Bernardine Evaristo</b> - <a href="https://www.goodreads.com/review/show/3503927235"><i>Girl, Woman, Other</i></a> (3/5)
+                </li>
+                <br></br>
+                <li data-block-key="o7g6x">
+                    <b>Peter May</b> - <a href="https://www.goodreads.com/review/show/3512621146"><i>Entry Island</i></a> (2/5)
+                </li>
+                <br></br>
+                <li data-block-key="5u10b">
+                    <b>Mariko Tamaki &amp; Jillian Tamaki</b> - <a href="https://www.goodreads.com/review/show/3521258130"><i>This One Summer</i></a> (2/5)
+                </li>
+                <br></br>
+                <li data-block-key="nz4v2">
+                    <b>Andrea Owen</b> - <a href="https://www.goodreads.com/review/show/3532989871"><i>How to Stop Feeling Like Shit: 14 Habits That Are Holding You Back From Happiness</i></a> (3/5)
+                </li>
+                <br></br>
+                <li data-block-key="7k6qm">
+                    <b>Tara June Winch</b> - <a href="https://www.goodreads.com/review/show/3425129488"><i>The Yield</i></a> (3/5)
+                </li>
+                <br></br>
+                <li data-block-key="45jw2">
+                    <b>Raven Leilani</b> - <a href="https://www.goodreads.com/review/show/3005271456"><i>Luster</i></a> (3/5)
+                </li>
+                <br></br>
+                <li data-block-key="bc28q">
+                    <b>Adam Buxton</b> - <a href="https://www.goodreads.com/review/show/3567345784"><i>Ramble Book: Musings on Childhood, Friendship, Family and 80s Pop Culture</i></a> (4/5)
+                </li>
+                <br></br>
+                <li data-block- key="n5a6w">
+                    <b>Olivia Wenzel</b> - <a href="https://www.goodreads.com/review/show/3217786816"><i>1000 Serpentinen Angst</i></a> (4/5 )
+                </li>
+                <br></br>
+                <li data-block-key="gmr1y">
+                    <b>Vivek Shraya</b> - <a href="https://www.goodreads.com/review/show/3570797532"><i>I&#x27;m Afraid of Men</i></a> (3/5)
+                </li>
+                <br></br>
+                <li data-block-key="6y28o">
+                    <b>Nicole Dennis-Benn</b> - <a href="https://www.goodreads.com/review/show/2859962170"><i>Patsy</i></a> (4/5)
+                </li>
+                <br></br>
+                <li data-block-key="go3im">
+                    <b>Tasha Spillett &amp; Natasha Donovan</b> - <a href="https://www.goodreads.com/review/show/3605822445"><i>Surviving the City</i></a> (4/5)
+                </li>
+                <br></br>
+                <li data-block-key="6xpus">
+                    <b>Zadie Smith</b> - <a href="https://www.goodreads.com/review/show/3617198097"><i>Intimations: Six Essays</i></a> (4/5)
+                </li>
+                <br></br>
+                <li data-block-key="wppav">
+                    <b>Emma Mitchell</b> - <a href="https://www.goodreads.com/review/show/3113347458"><i>The Wild Remedy: How Nature Mends Us</i></a> (4/5)
+                </li>
+                <br></br>
+                <li data-block-key="4fb8y">
+                    <b>Carolin Emcke (tr. Imogen Taylor)</b> - <a href="https://www.goodreads.com/review/show/2743946397"><i>How We Desire</i></a> (2/5)
+                </li>
+                <br></br>
+                <li data-block-key="rox2c">
+                    <b>Rachel Lynn Solomon</b> - <a href="https://www.goodreads.com/review/show/3617879160"><i>Today Tonight Tomorrow</i></a> (4/5)
+                </li>
+                <br></br>
+                <li data-block-key="z0ugw">
+                    <b>Raina Telgemeier</b> - <a href="https://www.goodreads.com/review/show/3626707607"><i>Smile</i></a>(4/5)
+                </li>
+                <br></br>
+                <li data-block-key="9vyl0">
+                    <b>T Kira Madden</b> - <a href="https://www.goodreads.com/review/show/3640865366"><i>Long Live the Tribe of Fatherless Girls</i></a> (4/5)
+                </li>
+                <br></br>
+                <li data-block-key="cb5fy">
+                    <b> Nadia Bolz-Weber</b> - <a href="https://www.goodreads.com/review/show/2833315573"><i>Shameless: A Sexual Reformation</i></a> (4/5)
+                </li>
+                <br></br>
+                <li data-block-key="x20dr">
+                    <b>John Kampfner</b> - <a href="https://www.goodreads.com/review/show/3691004579"><i>Why the Germans Do It Better: Notes from a Grown-Up Country</i></a> (4/5)
+                </li>
+                <br></br>
+                <li data-block-key="8r5w9">
+                    <b>Bethany Rutter</b> - <a href="https://www.goodreads.com/review/show/3692905404"><i>Melt My Heart</i></a> (4/5)
+                </li>
+                <br></br>
+                <li data-block-key="hmb02">
+                    <b>Helga Flatland (tr. Rosie Hedger) </b>- <a href="https://www.goodreads.com/review/show/3709216084"><i>A Modern Family</i></a> (2/5)
+                </li>
+                <br></br>
+                <li data-block-key="h15t4">
+                    <b>Julia Boyd</b> - <a href="https://www.goodreads.com/review/show/3709218784"><i>Travellers in the Third Reich: The Rise of Fascism Through the Eyes of Everyday People</i></a> (3/5)
+                </li>
+                <br></br>
+                <li data-block-key="rl450">
+                    <b>Leone Ross</b> - <a href="https://www.goodreads.com/review/show/3716415258"><i>Come Let Us Sing Anyway</i></a> (4/5)
+                </li>
+                <br></br>
+                <li data-block-key="z770h">
+                    <b>Nell Stevens</b> - <a href="https://www.goodreads.com/review/show/1992404214"><i>Bleaker House: Chasing My Novel to the End of the World</i></a> (3/5)
+                </li>
+</ol>
+
+
+
+
+          </div>
+      </article>
+        
+      </Container>
+    </Layout>
+    </>
+  )
+}
