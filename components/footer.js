b2d5df9ed304c721ea19b659ffad68cbe0c39e3c
Rosamund Mather <rsmnd.mthr@gmail.com>
2022-07-06 20:21:31 +0200

    Get rid of button

--- components/footer.js
+++ components/footer.js
@@ -48,19 +48,15 @@ export default function Footer() {
                   />
             </a> {''}
 
-
-
-
           </div>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <div
-              className="mx-3 text-sm"
+              className="text-2xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2"
             >
               <a
                 href="https://help-ua-volunteers.com/"
-                className="mx-1 text-center bg-black hover:bg-white hover:text-black border no-underline border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-4 lg:mb-0"
               >
-                🇺🇦 Help Ukrainian volunteers!
+                🇺🇦 Help volunteers!
               </a>
             </div>
           </div>
